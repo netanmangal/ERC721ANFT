6f6c576ece222eb041a782a594d5c5d8173437ff
netanmangal <imnetanmangal@gmail.com>
2022-03-16 18:35:10 +0530

    Add: Mint After

--- smart-contract/contracts/NFTContract.sol
+++ smart-contract/contracts/NFTContract.sol
@@ -7,9 +7,12 @@ import "./ERC721A.sol";
 import "./utils/AccessControl.sol";
 
 contract NFTContract is ERC721A, AccessControl {
-    constructor (string memory name_, string memory symbol_)
+    uint256 public mintingAllowedAfter;
+
+    constructor (string memory name_, string memory symbol_, uint256 mintingAllowedAfter_)
     ERC721A(name_, symbol_) {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        mintingAllowedAfter = block.timestamp + mintingAllowedAfter_;
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, AccessControl) returns (bool) {
@@ -18,6 +21,7 @@ contract NFTContract is ERC721A, AccessControl {
 
     function mint(uint256 quantity) external payable {
         // _safeMint's second argument now takes in a quantity, not a tokenId.
+        require(block.timestamp >= mintingAllowedAfter, "NFTContract Error: Minting not allowed yet.");
         _safeMint(msg.sender, quantity);
     }
 }
