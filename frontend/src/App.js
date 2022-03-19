baa187160547b817df1119451da6ee1a36a36e35
netanmangal <imnetanmangal@gmail.com>
2022-03-20 01:10:31 +0530

    Add: Check metamask installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,59 @@ import 'react-toastify/dist/ReactToastify.css';
 import logo from './logo.png';
 import './App.css';
 import Body from "./components/body.js";
+import web3 from "./web3.js";
+import contract from "./contract.js";
 
 function App() {
+  let [state, setState] = useState({
+    isMetamaskInstalled: false,
+    accounts: [],
+    tokenName: "",
+    tokenSymbol: ""
+  });
 
   useEffect(() => {
     toast.success("Welcome!!!");
-  });
+
+    const init = async () => {
+      setState({
+        ...state,
+        tokenName: await contract.methods.name().call(),
+        tokenSymbol: await contract.methods.symbol().call(),
+        accounts: await web3.eth.getAccounts()
+      });
+
+      console.log(state);
+    }
+    
+    const checkMetamask = async () => {
+      if (typeof web3 !== 'undefined') {
+        if (web3?.currentProvider?.isMetaMask === true) {
+          setState({
+            ...state,
+            isMetamaskInstalled: true
+          });
+          init();
+
+          if (state.accounts.length === 0) {
+            await window.ethereum.enable();
+          }
+        } else {
+          toast.error("Kindly install metamask!!! - 1");
+        }
+      } else {
+        toast.error("Kindly install metamask!!!");
+      }
+    }
+
+    checkMetamask();
+  }, []);
 
   return (
     <div className="App">
       <div className="App-body">
         <img style={{marginTop: "50px"}} src={logo} className="App-logo" alt="logo" />
         <h5>Made By: <a href="https://www.linkedin.com/in/netanmangal" target="_blank" rel="noreferrer noopener">Netan Mangal</a></h5>
-        <Body />
 
         <ToastContainer style={{fontSize: "1rem", width: "30rem"}} position="top-right" theme="dark" autoClose={3000} />
       </div>
