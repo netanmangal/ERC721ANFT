fef1836aa0c96e781029b9494fcbcf3797f5cc30
netanmangal <imnetanmangal@gmail.com>
2022-03-19 23:39:36 +0530

    Add: Body.js template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import logo from './logo.png';
 import './App.css';
+import Body from "./components/body.js";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <div className="App-body">
         <img style={{marginTop: "50px"}} src={logo} className="App-logo" alt="logo" />
         <h5>Made By: <a href="https://www.linkedin.com/in/netanmangal" target="_blank" rel="noreferrer noopener">Netan Mangal</a></h5>
+        <Body />
 
         <ToastContainer style={{fontSize: "1rem", width: "30rem"}} position="top-right" theme="dark" autoClose={3000} />
       </div>
