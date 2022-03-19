282fad9f4dbac64b04c2d06dc44ca042ad13514a
netanmangal <imnetanmangal@gmail.com>
2022-03-19 12:13:39 +0530

    Add: max number of purchases in one batch + error msg

--- smart-contract/contracts/NFTContract.sol
+++ smart-contract/contracts/NFTContract.sol
@@ -8,13 +8,12 @@ import "./utils/AccessControl.sol";
 
 contract NFTContract is ERC721A, AccessControl {
     uint256 public mintingAllowedAfter;
-    uint256 public reservedNFTs;
+    uint256 public maxPurchase = 20;
 
-    constructor (string memory name_, string memory symbol_, uint256 mintingAllowedAfter_, uint256 reservedNFTs_)
+    constructor (string memory name_, string memory symbol_, uint256 mintingAllowedAfter_)
     ERC721A(name_, symbol_) {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
         mintingAllowedAfter = block.timestamp + mintingAllowedAfter_;
-        reservedNFTs = reservedNFTs_;
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, AccessControl) returns (bool) {
@@ -24,10 +23,16 @@ contract NFTContract is ERC721A, AccessControl {
     function mint(uint256 quantity) external payable {
         // _safeMint's second argument now takes in a quantity, not a tokenId.
         require(block.timestamp >= mintingAllowedAfter, "NFTContract Error: Minting not allowed yet.");
+        require(
+            quantity <= maxPurchase, 
+            string(
+                abi.encodePacked(
+                    "NFTContract Error: Can't mint more than ",
+                    Strings.toString(maxPurchase),
+                    " NFTs"
+                )
+            )
+        );
         _safeMint(msg.sender, quantity);
-    }
-
-    function updateReservedNFTs (uint256 reservedNFTs_) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        reservedNFTs = reservedNFTs_;
     }
 }
